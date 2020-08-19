525a7b4b616c3cd47031c1cbf7b84828c3d71de1
fulviocanducci <fulviocanducci@hotmail.com>
2020-08-19 19:19:20 -0300

    Effect Default

--- index.tsx
+++ index.tsx
@@ -1,14 +1,25 @@
-import React, { useRef, FC} from 'react';
+import React, { useRef, FC, useState, useEffect} from 'react';
 import { render } from 'react-dom';
 import LoadingBar from 'react-top-loading-bar';
 
 const App: FC = () => {
   const ref = useRef(null)
+  const [progress, setProgress] = useState(0);
+  const handleProgress = () => setProgress(state => state + 1.6);
+  useEffect(() => {
+    setTimeout(() => handleProgress(), 1000);
+  }, [])
+  useEffect(() => {
+    if (progress < 100) {
+      setTimeout(() => handleProgress(), 1000);
+    }
+  }, [progress])
   return (
     <React.Fragment>
-      <LoadingBar color='#f11946' ref={ref} />
-      <div>App</div>
-      <button onClick={() => ref.current.complete()}>Complete</button>
+      <LoadingBar color='#f11946' 
+        progress={progress}
+        onLoaderFinished={() => handleProgress()} />  
+      {progress > 99 && <button onClick={e => setProgress(0)}>Reiniciar</button>}      
     </React.Fragment>
   )
 }
